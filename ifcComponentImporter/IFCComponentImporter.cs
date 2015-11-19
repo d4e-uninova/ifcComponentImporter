7db50394c84aa32b20ebdcae6c8a8da1c64f7255
Antonio Goncalves <amg@uninova.pt>
2015-11-19 12:46:55 +0000

    Test commit

--- ifcComponentImporter/IFCComponentImporter.cs
+++ ifcComponentImporter/IFCComponentImporter.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Autodesk.Revit.DB;
 using Autodesk.Revit.DB.IFC;
 using Autodesk.Revit.DB.Analysis;
